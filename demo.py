c8cb4955c26a27d5e357d6f4b27e4c0bf68a44d4
Harshal Jadhav <Harshal.Jadhav@pfizer.com>
2021-09-15 18:19:05 +0530

    added demo.py file

---
+++ demo.py
@@ -0,0 +1,6 @@
+message="""
+hello, this file was pushed from local to bitbucket server,
+if you're viewing this in databricks then it means the integration was successfull :)
+"""
+
+print(message)
