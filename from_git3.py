45d8a146dea81884a13c27513a7fa36d399da8c2
Harshal Jadhav <Harshal.Jadhav@pfizer.com>
2021-09-16 14:21:32 +0530

    added from_git3.py from local

---
+++ from_git3.py
@@ -0,0 +1,18 @@
+# lorem ipsun
+# asdasdf
+# asdfasdf
+# asdfasdf
+# asdfasdf
+# asdasdf
+# 
+# Databricks notebook source
+
+
+
+# lorem ipsum asdfasdfasdfasdfasdf
+# asdfadsfasfasd 
+# asdfasdfasdf
+# asdfasdfasdf
+# asdfasdf
+
+print("hello world from local 3 :)")
